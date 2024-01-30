33900ea3208cfdab491e3999b46b134217a59587
lueberGandra <lueber_emp@hotmail.com>
2024-01-30 00:32:03 -0300

    feat: adds barbershop component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        //remotePatterns: ['https://utfs.io']
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'utfs.io',
+                    port: '',
+                },
+            ],
+    }
+};
 
 export default nextConfig;
